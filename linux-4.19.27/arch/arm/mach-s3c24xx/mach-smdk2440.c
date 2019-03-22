f7d600ce7bef969fdff154659bab700193cca693
flinn <flinn682@foxmail.com>
2019-03-22 14:23:14 +0800

    linux-4.19.27 : add dm9000, nfs mount file

--- linux-4.19.27/arch/arm/mach-s3c24xx/mach-smdk2440.c
+++ linux-4.19.27/arch/arm/mach-s3c24xx/mach-smdk2440.c
@@ -40,6 +40,10 @@
 #include "common.h"
 #include "common-smdk.h"
 
+#include <linux/dm9000.h>
+
+#define MACH_SMDK2440_DM9K_BASE (S3C2410_CS4 + 0x300)
+
 static struct map_desc smdk2440_iodesc[] __initdata = {
 	/* ISA IO Space map (memory space selected by A24) */
 
@@ -142,12 +146,43 @@ static struct s3c2410fb_mach_info smdk2440_fb_info __initdata = {
 	.lpcsel		= ((0xCE6) & ~7) | 1<<4,
 };
 
+
+/*
+* DM9000 add by Flinn for mini2440
+*/
+static struct resource smdk2440_dm9k_resource[] = {
+        [0] = DEFINE_RES_MEM(MACH_SMDK2440_DM9K_BASE, 4),
+        [1] = DEFINE_RES_MEM(MACH_SMDK2440_DM9K_BASE + 4, 4),
+        [2] = DEFINE_RES_NAMED(IRQ_EINT7, 1, NULL, IORESOURCE_IRQ \
+                                                | IORESOURCE_IRQ_HIGHEDGE),
+};
+
+/*
+ * The DM9000 has no eeprom, and it's MAC address is set by
+ * the bootloader before starting the kernel.
+ */
+static struct dm9000_plat_data smdk2440_dm9k_pdata = {
+        .flags          = (DM9000_PLATF_16BITONLY | DM9000_PLATF_NO_EEPROM),
+};
+
+static struct platform_device smdk2440_device_eth = {
+        .name           = "dm9000",
+        .id             = -1,
+        .num_resources  = ARRAY_SIZE(smdk2440_dm9k_resource),
+        .resource       = smdk2440_dm9k_resource,
+        .dev            = {
+                .platform_data  = &smdk2440_dm9k_pdata,
+        },
+};
+
+
 static struct platform_device *smdk2440_devices[] __initdata = {
 	&s3c_device_ohci,
 	&s3c_device_lcd,
 	&s3c_device_wdt,
 	&s3c_device_i2c0,
 	&s3c_device_iis,
+	&smdk2440_device_eth,
 };
 
 static void __init smdk2440_map_io(void)
